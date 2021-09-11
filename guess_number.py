731a56ff873e347430278cb28c19cb527df8c6c4
CKC <chinkuanchen@gmail.com>
2021-09-12 01:07:31 +0800

    add count

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,11 @@
 import random
 r = random.randint(1, 100)
+n = 0
 while r > 0:
+    n += 1  #n = n + 1
     guess = input('請輸入數字1~100整數：')
     guess = int(guess)
+    print('這是你猜的第', n, '次')
     if guess == r:
         print('猜對了！')
         break
@@ -12,3 +15,5 @@ while r > 0:
         print('太大了喔！')
     else:
         print('數字範圍錯誤！')
+    
+
